cceb6e3e21d7c7f47860b7c9e9e410e5a3132bd5
Agrougroum <guimezanes.benoit@gmail.com>
2024-07-29 15:32:08 +0200

    cv-vault

--- apps/cv-vault/src/app/app.component.ts
+++ apps/cv-vault/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ResumePageComponent } from './pages/resume-page/resume-page.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
+  imports: [ResumePageComponent, RouterModule],
+  selector: 'cvv-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ apps/cv-vault/src/app/pages/resume-page/resume-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cvv-resume-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './resume-page.component.html',
+  styleUrl: './resume-page.component.scss',
+})
+export class ResumePageComponent {}
