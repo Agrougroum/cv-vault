4dd6e887139f2aaf8012d7c692fbf0527c40edf2
Agrougroum <guimezanes.benoit@gmail.com>
2024-07-29 16:04:11 +0200

    add material

--- apps/cv-vault/src/app/app.config.ts
+++ apps/cv-vault/src/app/app.config.ts
@@ -2,11 +2,12 @@ import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
     provideRouter(appRoutes),
-    provideExperimentalZonelessChangeDetection(),
+    provideExperimentalZonelessChangeDetection(), provideAnimationsAsync(),
   ],
 };
