c6c02fba71ca575345b4eb56e9f3d846b25cf01a
Agrougroum <guimezanes.benoit@gmail.com>
2024-07-30 16:13:40 +0200

    style customization

--- apps/cv-vault/src/app/app.config.ts
+++ apps/cv-vault/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+// import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(),
+    // provideClientHydration(),
     provideRouter(appRoutes),
     provideExperimentalZonelessChangeDetection(), provideAnimationsAsync(),
   ],
