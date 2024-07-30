63dc2efe04769f8bc2629af67210f7218df4d2f9
Agrougroum <guimezanes.benoit@gmail.com>
2024-07-30 14:25:51 +0200

    layout + shared css

---
+++ apps/cv-vault/src/app/pages/resume-page/components/main-intro/main-intro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cvv-main-intro',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-intro.component.html',
+  styleUrl: './main-intro.component.scss',
+})
+export class MainIntroComponent {}
--- apps/cv-vault/src/app/pages/resume-page/resume-page.component.ts
+++ apps/cv-vault/src/app/pages/resume-page/resume-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainIntroComponent } from './components/main-intro/main-intro.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'cvv-resume-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MainIntroComponent, AboutComponent],
   templateUrl: './resume-page.component.html',
   styleUrl: './resume-page.component.scss',
 })
-export class ResumePageComponent {}
+export class ResumePageComponent { }
